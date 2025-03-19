5ff578cf055daa9a5e51a382805ef3a5dea67164
Ana Chiaramonte <anasenai.lopes@gmail.com>
2025-03-19 10:50:15 -0300

    Fazendo o fundo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "./App.css"
+import Nuvem from "./assets/Design sem nome (9).png"
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div id="app">
+ 
+    <img className="Nuvem" src={Nuvem} alt="" />
+  </div>;
+};
 
-export default App
-
+export default App;
