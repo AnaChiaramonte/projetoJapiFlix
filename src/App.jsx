6e140176c4cec75834be39b9a5c8598c12c2ecbd
Ana Chiaramonte <anasenai.lopes@gmail.com>
2025-03-19 11:59:01 -0300

    fazendo a barra de pesquisa

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "./App.css"
-import Nuvem from "./assets/Design sem nome (9).png"
+
+import Nuvem from "./assets/Design sem nome (11).png"
 
 const App = () => {
-  return <div id="app">
- 
+
+
+
+  return (
+     <div id="app">
     <img className="Nuvem" src={Nuvem} alt="" />
-  </div>;
+   
+        <input className="form-control mt-n5 m-5"
+          
+          type="text"
+          placeholder="Pesquisa por filme..."
+        />
+    
+    </div>
+    
+  );
 };
 
 export default App;
