534151a9351088813181b5b64937604c22ef94b2
Ana Geremias <anageremiias@gmail.com>
2025-03-19 17:00:35 -0300

    Mudanças no link do Pedro e cores

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "./App.css"
-
 import Nuvem from "./assets/Design sem nome (11).png"
-import "./App.css";
-import Nuvem from "./assets/Design sem nome (9).png";
 import Footer from "./components/footer/Footer";
 
 const App = () => {
@@ -21,23 +18,18 @@ const App = () => {
           placeholder="Pesquisa por filme..."
         />
     
-    </div>
-    
-  );
-  return (
-    <div id="app">
-      <img className="Nuvem" src={Nuvem} alt="" />
-
-      <Footer
-        devn1={"Ana Geremias"}
-        devL1={"https://github.com/Najul1a"}
-        devn2={"Ana Lopes"}
-        devL2={"https://github.com/AnaChiaramonte"}
-        devn3={"Pedro Araujo"}
-        devL3={"https://github.com/Najul1a"}
-      />
-    </div>
-  );
+  
+    <Footer
+      devn1={"Ana Geremias"}
+      devL1={"https://github.com/Najul1a"}
+      devn2={"Ana Lopes"}
+      devL2={"https://github.com/AnaChiaramonte"}
+      devn3={"Pedro Araujo"}
+      devL3={"https://github.com/PedroAraujosz"}
+    />
+  </div>
+)
 };
+  
 
 export default App;
