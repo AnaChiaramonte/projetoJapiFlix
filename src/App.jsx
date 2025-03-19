d1adf67c6c654308369d1987e530c572bdebaf4b
Ana Geremias <anageremiias@gmail.com>
2025-03-19 16:09:03 -0300

    Fazendo o Footer com links

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
-import "./App.css"
-import Nuvem from "./assets/Design sem nome (9).png"
+import "./App.css";
+import Nuvem from "./assets/Design sem nome (9).png";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
-  return <div id="app">
- 
-    <img className="Nuvem" src={Nuvem} alt="" />
-  </div>;
+  return (
+    <div id="app">
+      <img className="Nuvem" src={Nuvem} alt="" />
+
+      <Footer
+        devn1={"Ana Geremias"}
+        devL1={"https://github.com/Najul1a"}
+        devn2={"Ana Lopes"}
+        devL2={"https://github.com/AnaChiaramonte"}
+        devn3={"Pedro Araujo"}
+        devL3={"https://github.com/Najul1a"}
+      />
+    </div>
+  );
 };
 
 export default App;
