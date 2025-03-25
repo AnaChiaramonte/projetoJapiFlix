23ef24460910581fa33eef1525ad465cc24061f8
Ana Chiaramonte <anasenai.lopes@gmail.com>
2025-03-25 11:43:54 -0300

    arrumando o appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,73 +10,78 @@ import Switch from "./components/switch/Switch"
     
   
 
-  const App = () => {
-    const [isLight, setIsLight] = useState(true);
-    const troca = () => {
-      setIsLight((anterior) => !anterior)
-      
-    }
+const App = () => {
+  const [isLight, setIsLight] = useState(true);
+  const troca = () => {
+    setIsLight((anterior) => !anterior);
+  };
+
   const [search, setSearch] = useState("");
   const [movies, setMovies] = useState([]);
-  // const apiKey = "";
-  // const apiUrl = ``;
+
+  const apiKey = "e4d577fa";
+  const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
+
   useEffect(() => {
     searchMovies("Batman");
   }, []);
+
   const searchMovies = async (title) => {
     const response = await fetch(`${apiUrl}&s=${title}`);
     const data = await response.json();
     setMovies(data.Search);
-  }
-  return (
-        
-    <div id="App" className={isLight ? "light" : ""}>
+  };
 
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") searchMovies(search);
+  };
+
+  // O return tem que estar aqui dentro!
+  return (
+    <div id="App" className={isLight ? "light" : ""}>
       <Switch troca={troca} isLight={isLight} />
-    <div
-      id="app"
-      className="d-flex flex-column justify-content-center align-items-center w-100"
-    >
-      <img className="Nuvem w-100" src={Nuvem} alt="" />
-      <div className="d-flex w-75 m-0 mt-4 mb-2 align-items-center justify-content-between p-4 rounded-pill corPesquisa">
-        <input
-          className="bg-transparent border-0 fs-2 outLine align-self-baseline w-100"
-          type="text"
-          onChange={(e) => setSearch(e.target.value)}
-          type="text"
-          placeholder="Pesquisa por filme..."
+      <div
+        id="app"
+        className="d-flex flex-column justify-content-center align-items-center w-100"
+      >
+        <img className="Nuvem w-100" src={Nuvem} alt="" />
+        <div className="d-flex w-75 m-0 mt-4 mb-2 align-items-center justify-content-between p-4 rounded-pill corPesquisa">
+          <input
+            className="bg-transparent border-0 fs-2 outLine align-self-baseline w-100"
+            onKeyDown={handleKeyPress}
+            type="text"
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Pesquisa por filme..."
+          />
+          <img onClick={() => searchMovies(search)} src={lupa} alt="" />
+        </div>
+        <MovieCards 
+          Title="Inception" 
+          Year="2010" 
+          Type="Movie" 
+          Poster="https://via.placeholder.com/300x450" 
+          imdbID="tt1375666"
         />
-        <img
-          className=""
-          onClick={() => searchMovies(search)}
-          
-          src={lupa}
-          alt=""
-         
+        {movies?.length > 0 ? (
+          <div className="container">
+            {movies.map((movie, index) => (
+              <MovieCards key={index} apiUrl={apiUrl} {...movie} />
+            ))}
+          </div>
+        ) : (
+          <h2 className="empty">😢 Filme não encontrado 😢</h2>
+        )}
+        <Footer
+          devn1={"Ana Geremias"}
+          devL1={"https://github.com/Najul1a"}
+          devn2={"Ana Lopes"}
+          devL2={"https://github.com/AnaChiaramonte"}
+          devn3={"Pedro Araujo"}
+          devL3={"https://github.com/PedroAraujosz"}
         />
       </div>
-      <MovieCards 
-  Title="Inception" 
-  Year="2010" 
-  Type="Movie" 
-  Poster="https://via.placeholder.com/300x450" 
-  imdbID="tt1375666"
-/>
-
-    
-  
-    <Footer
-      devn1={"Ana Geremias"}
-      devL1={"https://github.com/Najul1a"}
-      devn2={"Ana Lopes"}
-      devL2={"https://github.com/AnaChiaramonte"}
-      devn3={"Pedro Araujo"}
-      devL3={"https://github.com/PedroAraujosz"}
-    />
-  </div>
-  </div>
-);
-  }
-
+    </div>
+  );
+};
 
 export default App;
