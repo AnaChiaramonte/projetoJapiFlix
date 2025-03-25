dd68631fba45bb5c426f79640f2da099eefdb0cd
Ana Geremias <anageremiias@gmail.com>
2025-03-25 14:11:59 -0300

    mudanças

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import lupa from "./assets/Lupa.svg";
 import MovieCards from "./movieCards/MovieCards";
-import Nuvem from "./assets/Design sem nome (11).png";
-import Nuvem from "./assets/Nuvem.png"
+import Nuvem from "./assets/Nuvem.png";
 import Footer from "./components/footer/Footer";
 import Japiflix from "./assets/J.png";
 import Switch from "./components/switch/Switch"
